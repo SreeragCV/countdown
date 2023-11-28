4d4847da8f61fbd7ac8444692580ff09f16a7bf7
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-11-28 16:45:11 +0530

    useRefs, setTimeOuts and clearTimeOuts added

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-export default function TimerChallenge({title, targetTime}) {
+export default function TimerChallenge({ title, targetTime }) {
+  const [timeExpired, setTimeExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const timer = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop(){
+    clearTimeout(timer.current)
+    // setTimeExpired(true)
+    // setTimerStarted(false)
+  }
+
   return (
-    <section className='challenge'>
+    <section className="challenge">
       <h2>{title}</h2>
-      <p className='challenge-time'>
-        {targetTime} second{targetTime > 1 ? 's' : ''}
+      {timeExpired && <p>You Lost</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>
-            Start Challenge
-        </button>
+        <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
       </p>
-      <p>
-        Time is running... / Timer In 
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? "Time Is Running" : "Time Inactive"}
       </p>
     </section>
-  )
+  );
 }
